64c9fc2a6160ab404342007361705ec1c8ea853c
Luke <lukewashburn89@gmail.com>
2020-09-28 20:35:52 -0500

    handled case of random.org not working

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,44 @@
 let RandomOrg = require('random-org');
 
 export class App {
-  public message = 'Number of Successes';
+  public successes = 'Number of Successes';
 
 
 public numD6 = 0;
 public numD4 = 0;
 private totalSuccesses = 0;
 
-  private async getRandomInt(min: number, max: number, count: number) {
+  private async getRandomIntsFromOneToMax(max: number, count: number) {
 
     if (count > 30){
       console.error('TOO MANY DICE');
       
     }else{
       let random = new RandomOrg({ apiKey: 'ab5f7285-94f1-4c60-8b1d-a8d4a343745b' });
-      let randNumInRange = await random.generateIntegers({ min: min, max: max, n: count}).then((result: any) => {
+      let randNumInRange = await random.generateIntegers({ min: 1, max: max, n: count}).then((result: any) => {
         return result.random.data;
-      })
+      }, (issue: any) => {
+        // if random.org fails then log it and return a psuedo random number array
+        console.log(issue);
+        let numArr: number[] = [];
+        for(let i = count; i > 0 ; i--){
+          // only gives numbers from 1 to max 
+          numArr.push(Math.floor(Math.random() * max) + 1)
+        }
+        return  numArr;
+
+      }
+      );
       return randNumInRange;
     }
 
   }
 
   private async onClickRollDice(){
-    this.message = 'Rolling...';
+    this.successes = 'Rolling...';
     this.totalSuccesses = await this.rollDice(this.numD6, this.numD4)
 
-    this.message = this.totalSuccesses.toString();
+    this.successes = this.totalSuccesses.toString();
 
   }
 
@@ -37,7 +48,7 @@ private totalSuccesses = 0;
     let successes = 0;
 
     if(d6s > 0){
-      let d6Array = await this.getRandomInt(1, 6, d6s);
+      let d6Array = await this.getRandomIntsFromOneToMax(6, d6s);
       d6Array.forEach(element => {
         if (element == 1){
           successes++;
@@ -46,7 +57,7 @@ private totalSuccesses = 0;
   }
 
   if(d4s > 0){
-    let d4Array = await this.getRandomInt(1, 4, d4s);
+    let d4Array = await this.getRandomIntsFromOneToMax(4, d4s);
     d4Array.forEach(element => {
       if (element == 1){
         successes++;
